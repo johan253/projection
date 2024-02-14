01c6119f84d0b6d57be7bc5044024f034e8f35d1
johan253 <johannjo2000@gmail.com>
2024-02-13 21:34:36 -0800

    Added logo to Stage

--- src/main/java/johan/projector/App.java
+++ src/main/java/johan/projector/App.java
@@ -7,14 +7,15 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/main.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
-        //stage.getIcons().add(new Image("johan/projector/logo.png"));
+        stage.setTitle("Projector");
+        stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/assets/logo.png"))));
         stage.setScene(scene);
         stage.show();
     }
