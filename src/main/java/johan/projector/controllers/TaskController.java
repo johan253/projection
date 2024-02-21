3c1a7ac72aa4e1af5d30b443b9df105a8d70920c
johan253 <johannjo2000@gmail.com>
2024-02-21 09:51:17 -0800

    Added Initializable implementation

--- src/main/java/johan/projector/controllers/TaskController.java
+++ src/main/java/johan/projector/controllers/TaskController.java
@@ -2,15 +2,19 @@ package johan.projector.controllers;
 
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import johan.projector.models.Task;
 import johan.projector.models.TaskStatus;
 
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
 
-public class TaskController {
+public class TaskController implements Initializable {
     @FXML
     private Label taskTitleLabel;
     @FXML
@@ -41,5 +45,10 @@ public class TaskController {
     }
     private void deleteClick() {
         System.out.println("deleting task...");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println(resourceBundle.toString());
     }
 }
