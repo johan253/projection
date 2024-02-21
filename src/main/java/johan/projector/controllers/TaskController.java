986956a4ede0e906a26f1ec7fba1ea86d0219ef9
johan253 <johannjo2000@gmail.com>
2024-02-21 09:46:45 -0800

    Added Task Controller

--- src/main/java/johan/projector/controllers/TaskController.java
+++ src/main/java/johan/projector/controllers/TaskController.java
@@ -1,4 +1,52 @@
 package johan.projector.controllers;
 
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import johan.projector.models.DatabaseDriver;
+import johan.projector.models.Project;
+import johan.projector.models.Task;
+import johan.projector.models.TaskStatus;
+
+import java.beans.PropertyChangeSupport;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
+
 public class TaskController {
+    @FXML
+    private Label taskTitleLabel;
+    @FXML
+    private ChoiceBox<String> statusChoiceBox;
+    @FXML
+    private FontAwesomeIconView deleteIcon;
+    private Task myTask;
+    public void setTask(final Task theTask) {
+        deleteIcon.setOnMouseClicked(e -> deleteClick());
+        myTask = theTask;
+        taskTitleLabel.setText(myTask.getTitle());
+        statusChoiceBox.setItems(FXCollections.observableList(List.of("Unfinished", "Finished", "In Progress")));
+        statusChoiceBox.setValue(switch(theTask.getStatus()) {
+            case UNFINISHED -> "Unfinished";
+            case INPROGRESS -> "In Progress";
+            case FINISHED -> "Finished";
+        });
+        statusChoiceBox.setOnAction(e -> changeStatus());
+    }
+
+    public void changeStatus() {
+        TaskStatus status = switch (statusChoiceBox.getValue()) {
+            case "Finished" -> TaskStatus.FINISHED;
+            case "In Progress" -> TaskStatus.INPROGRESS;
+            default -> TaskStatus.UNFINISHED;
+        };
+        myTask.setStatus(status);
+    }
+    private void deleteClick() {
+        System.out.println("deleting task...");
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module johan.projector {
     requires javafx.base;
     requires java.sql;
     requires java.desktop;
+    requires de.jensd.fx.glyphs.fontawesome;
 
 
     opens johan.projector to javafx.fxml;
